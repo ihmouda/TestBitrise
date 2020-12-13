ab3c2aa2f4380e7bd77746454c021818f58e65ca
asal.mhmouda@rmdy.health <mihmouda>
2020-12-13 09:35:40 +0200

    first commit

---
+++ TestBitriseTests/TestBitriseTests.swift
@@ -0,0 +1,31 @@
+//
+//  TestBitriseTests.swift
+//  TestBitriseTests
+//
+//  Created by mihmouda on 12/13/20.
+//
+
+import XCTest
+
+class TestBitriseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(95, 95, "The are both equal")
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
